fe1d2f323bc370a2ba1c0485c325ef693eecf841
Alex <al.haruca@gmail.com>
2022-08-10 12:50:45 +0200

    fix useEffect

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,15 +4,21 @@ import { Item } from '../Item';
 import styles from './List.module.css';
 
 export const List = () => {
-  const defaultList = { items: [] };
-  const [list, setList] = useState(defaultList);
+  const [list, setList] = useState(null);
   const [itemContent, setItemContent] = useState('');
   const [refresh, setRefresh] = useState(true);
 
   useEffect(() => {
-    if (list !== defaultList) {
+    if (list !== null) {
       localStorage.setItem('list', JSON.stringify(list));
-    } else {
+    }
+
+    if (localStorage.getItem('list') === null) {
+      localStorage.setItem('list', JSON.stringify({ items: [] }));
+      setList({ items: [] });
+    }
+
+    if (localStorage.getItem('list') && list === null) {
       setList(JSON.parse(localStorage.getItem('list')));
     }
   }, [refresh]);
@@ -32,7 +38,7 @@ export const List = () => {
     <>
       <div className={styles.list__container}>
         <ul className={styles.list}>
-          {list.items.map((thisItem, index, thisArr) => (
+          {list !== null && list.items.map((thisItem, index, thisArr) => (
             <li key={thisItem.id}>
               <Item
                 item={thisItem}
